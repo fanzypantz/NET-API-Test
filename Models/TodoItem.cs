5529b3e331f1934f02a25b736d4d06bdc29f2b7a
Andreas Halvorsen Tollånes <andreas@tollanes.com>
2020-09-16 14:25:54 +0200

    Initial commit

---
+++ Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoTest.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public long UserId { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
